601007d6dd054b8b2aab46bcbe0854271931c9c9
soyen0661 <161974535+soyen0661@users.noreply.github.com>
2024-04-19 20:43:01 +0900

    Update commentSchema text maxLength to 50

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const commentSchema = new mongoose.Schema({
-  text: { type: String, required: true, maxLength: 20 },
+  text: { type: String, required: true, maxLength: 50 },
   owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
   createdAt: { type: Date, required: true, default: Date.now },
   video: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Video" },
